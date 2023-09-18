76cec7c5867b186bbe54c4cef5f455a5461b712b
Ghost <108940365+vinibr33@users.noreply.github.com>
2023-09-18 08:05:51 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 mensagem = (msg) => alert(msg);
 soma = (a,b) => a + b;
+sub = (a,b) => a - b;
+mult = (a,b) => a * b;
+div = (a,b) => a / b;
+raiz = x => Math.sqrt(x);
+// mensagem(soma(10,soma(8,2)));
+// mensagem(soma(2,div(2,2)))
+// mensagem(soma(div(80,2),mult(5,3)));
+// mensagem(soma(sub(8,5),sub(3,10)));
