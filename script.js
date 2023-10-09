8cc7b361ca73861bc0f06ea08ab75596dc4a8b4d
Ghost <108940365+vinibr33@users.noreply.github.com>
2023-10-09 08:31:11 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,36 +4,73 @@ sub = (a,b) => a - b;
 mult = (a,b) => a * b;
 div = (a,b) => a / b;
 raiz = x => Math.sqrt(x);
+
 let a = "";
 let b = "";
 let valor = "";
 let temPonto = false;
-//Função para mostrar números no display
-function escrever_display(num){
-    document.getElementById("resultado").value = num;
+let desligada = true;
+
+function porcentagem(){
+    if(executa == "mult"){
+        b = valor;
+        escrever_display(div(mult(a,b),100));
+        b = "";
+        valor = "";
+    }
 }
 
-//Função para receber as teclas númericas e o ponto.
+function raiz_quadrada(){
+    escrever_display(raiz(valor));
+    valor = "";
+}
+
+function onoff(){
+    if(desligada){
+        desligada = false;
+        zerar();
+    }else{
+        zerar();
+        escrever_display("");
+        desligada = true;
+    }
+}
+
+function zerar(){
+    if(desligada) return;
+    a = "";
+    b = "";
+    valor = "";
+    temPonto = "";
+    escrever_display(0);
+}
+
+function escrever_display(num){
+    document.getElementById("resultado").value=num;
+}
 function digitando(tecla){
-    if(tecla == "."){
+   if(desligada) return;
+   if( tecla == "."){
         if(!temPonto){
             temPonto = true;
             valor += tecla;
             escrever_display(valor);
         }
         return;
-    }
-    valor += tecla;
-    escrever_display(valor);
+   }
+   valor += tecla;
+   escrever_display(valor);
 }
 let executa = "";
 function operacao(op){
+    if(desligada) return;
     executa = op;
     a = valor;
     valor = "";
     temPonto = false;
 }
 function calcula(){
+    if(desligada) return;
     if(executa != ""){
         b=valor;
         if(executa == "mult") escrever_display(mult(a,b));
